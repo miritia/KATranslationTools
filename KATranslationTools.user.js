1c1f14b570e5b722aae3a3685b94e3f99ae3e7f9
miritia <martusgig@gmail.com>
2016-08-20 12:07:25 +0200

    Update KATranslationTools.user.js
    not completed yet!

--- KATranslationTools.user.js
+++ KATranslationTools.user.js
@@ -26,27 +26,23 @@
     */
    function altQAction() {
         //Dollary
-        simpleReplaceInTxtbox(/\$\\\\\$(-?\d+([.,]\d+)?)\$/g, "$$$1{\\,}€\$");
+        simpleReplaceInTxtbox(/\$\\\\\$(-?\d+([.,]\d+)?)\$/g, "$$$1{\\,}z\u0142\$");
         //Decimal dot to comma
         simpleReplaceInTxtbox(/(-?\d+\}?)\.(-?\d+|\\\\[a-z]+\{\d+)/g, "$1{,}$2");
         //In number: Need {\,} instead of {\ }
         simpleReplaceInTxtbox(/(\d+)\{\\ \}(\d+)/g, "$1{\\,}$2");
         //In number: Need percentage
         simpleReplaceInTxtbox(/\\*\s*%\s*\$/g, "{\\,}\\%$$");
-        //daß -> dass
-        simpleReplaceInTxtbox(/daß/g, "dass");
-        //daß -> dass
-        simpleReplaceInTxtbox(/\.\s*\*\s+\*/g, ".**");
         //40€ -> 40{\,}€ (simple cases)
         simpleReplaceInTxtbox(/\$(\d+)€/g, "$$$1{\\,}€");
         simpleReplaceInTxtbox(/(\d+)€$/g, "$1{\\,}€$$");
         //x-intercept / y-intercept
-        simpleReplaceInTxtbox(/\$x\$-intercept/g, "Schnittpunkt mit der $$x$$-Achse");
-        simpleReplaceInTxtbox(/\$y\$-intercept/g, "Schnittpunkt mit der $$y$$-Achse");
+        simpleReplaceInTxtbox(/\$x\$-intercept/g, "przcięcie z osią $$X$$");
+        simpleReplaceInTxtbox(/\$y\$-intercept/g, "przecięcie z osią $$Y$$");
         //and -> und and other simple phrase replacements
-        simpleReplaceInTxtbox(/Both ([A-Z][a-z]+) and ([A-Z][a-z]+)/g, "Sowohl $1 als auch $2");
-        simpleReplaceInTxtbox(/Yes, (.*) is correct but (.*) is not(\.?)/g, "Ja, $1 liegt richtig, aber $2 nicht.");
-        simpleReplaceInTxtbox(/^In conclusion, the equation has one solution:/g, "Zusammenfassend hat die Gleichung eine Lösung:");
+        simpleReplaceInTxtbox(/Both ([A-Z][a-z]+) and ([A-Z][a-z]+)/g, "Zarówno $1 jak i $2");
+        simpleReplaceInTxtbox(/Yes, (.*) is correct but (.*) is not(\.?)/g, "Tak, $1 jest prawidłowe, ale $2 nie.");
+        simpleReplaceInTxtbox(/^In conclusion, the equation has one solution:/g, "Podsumowując, równanie ma jedno rozwiązaniee:");
         simpleReplaceInTxtbox(/Find the complex conjugate (.*) of/g, "Finde die Konjugierte $1 von");
         simpleReplaceInTxtbox(/Your answer should be a complex number in the form \$a\+bi\$ where \$a\$ and \$b\$ are real numbers./g, "Deine Antwort sollte eine Komplexe Zahl im Format $$a+bi$$ sein, wobei $$a$$ und $$b$$ reelle Zahlen sind.");
         simpleReplaceInTxtbox(/What are the real and imaginary parts of (\$[a-z]\$) ?\?/g, "Was ist der Real- und Imaginärteil von $1 ?");
@@ -54,47 +50,47 @@
         simpleReplaceInTxtbox(/Is the matrix (\$[A-Z]\$) invertible\?/g, "Ist die Matrix $1 invertierbar?");
         simpleReplaceInTxtbox(/The answer is(:?)/g, "Die Antwort ist\1");
         simpleReplaceInTxtbox(/The answer(:?)/g, "Die Antwort$1");
-        simpleReplaceInTxtbox(/ is not a factor of /g, " ist kein Faktor von ");
-        simpleReplaceInTxtbox(/In conclusion,/g, "Zusammenfassend gilt:");
-        simpleReplaceInTxtbox(/To conclude\s*[:,]?(\s*)/g, "Zusammenfassend gilt: ");
-        simpleReplaceInTxtbox(/This is because/g, "Das gilt, weil");
-        simpleReplaceInTxtbox(/\bunits\b/g, "Einheiten");
-        simpleReplaceInTxtbox(/What type of number is/g, "Welche Art Zahl ist");
-        simpleReplaceInTxtbox(/Pythagorean theorem/g, "Satz des Pythagoras");
-        simpleReplaceInTxtbox(/Step(\s+\d+)/g, "Schritt\1");
-        simpleReplaceInTxtbox(/[Ss]quare root/g, "Quadratwurzel");
-        simpleReplaceInTxtbox(/[Cc]ube root/g, "Kubikwurzel");
-        simpleReplaceInTxtbox(/Let's start by replacing (.*) (by|with)/g, "Beginnen wir mit dem Ersetzen von $1 durch");
-        simpleReplaceInTxtbox(/Conclusion(:?) ?/g, "Schlussfolgerung\1");
-        simpleReplaceInTxtbox(/Summary(:?) ?/g, "Zusammenfassung\1");
-        simpleReplaceInTxtbox(/Express your answer in the form/g, "Schreibe deine Antwort in der Form");
-        simpleReplaceInTxtbox(/ is real and complex/g, " ist eine reale und komplexe Zahl");
-        simpleReplaceInTxtbox(/ is pure imaginary and complex/g, " ist rein imaginär und daher eine komplexe Zahl");
-        simpleReplaceInTxtbox(/ is complex/g, " ist eine komplexe Zahl");
-        simpleReplaceInTxtbox(/ unit /g, " Einheit");
-        simpleReplaceInTxtbox(/ units /g, " Einheiten ");
-        simpleReplaceInTxtbox(/ units$/g, " Einheiten");
-        simpleReplaceInTxtbox(/ unit$/g, " Einheit");
-        simpleReplaceInTxtbox(/\bAnswer\b/g, "Antwort");
-        simpleReplaceInTxtbox(/ and so /g, ", also gilt: ");
-        simpleReplaceInTxtbox(/ and /g, " und ");
-        simpleReplaceInTxtbox(/ or /g, " oder ");
-        simpleReplaceInTxtbox(/\bThis is\b/g, "Das ist");
-        simpleReplaceInTxtbox(/\bSolving for\b/g, "Auflösen nach");
-        simpleReplaceInTxtbox(/ for /g, " für ");
-        simpleReplaceInTxtbox(/ converges/g, " konvergiert");
-        simpleReplaceInTxtbox(/ diverges/g, " divergiert");
-        simpleReplaceInTxtbox(/Only/g, "Nur");
-        simpleReplaceInTxtbox(/\bYes\b/g, "Ja");
-        simpleReplaceInTxtbox(/\byes\b/g, "ja");
-        simpleReplaceInTxtbox(/\bNo\b/g, "Nein");
-        simpleReplaceInTxtbox(/\bno\b/g, "nein");
-        simpleReplaceInTxtbox(/\bbut\b/g, "aber");
-        simpleReplaceInTxtbox(/\bBut\b/g, "Aber");
-        simpleReplaceInTxtbox(/\bpoints\b/g, "Punkte");
-        simpleReplaceInTxtbox(/\bpoint\b/g, "Punkt");
-        simpleReplaceInTxtbox(/\b[Ss]catterplot\b/g, "Streudiagramm");
-        simpleReplaceInTxtbox(/\bFill in the blank\b/g, "Fülle den Platzhalter aus");
+        simpleReplaceInTxtbox(/ is not a factor of /g, " nie jest czynnikiem ");
+        simpleReplaceInTxtbox(/In conclusion,/g, "Podsumowując:");
+        simpleReplaceInTxtbox(/To conclude\s*[:,]?(\s*)/g, "Podsumowując: ");
+        simpleReplaceInTxtbox(/This is because/g, "Jest tak, ponieważ");
+        simpleReplaceInTxtbox(/\bunits\b/g, "jednostek");
+        simpleReplaceInTxtbox(/What type of number is/g, "Jakim rodzajem liczby jest");
+        simpleReplaceInTxtbox(/Pythagorean theorem/g, "Twierdzenie Pitagorasa");
+        simpleReplaceInTxtbox(/Step(\s+\d+)/g, "Krok\1");
+        simpleReplaceInTxtbox(/[Ss]quare root/g, "Pierwiastek kwadratowy");
+        simpleReplaceInTxtbox(/[Cc]ube root/g, "Pierwiastek sześcienny");
+        simpleReplaceInTxtbox(/Let's start by replacing (.*) (by|with)/g, "Zacznijmy od podstawienia $1 za");
+        simpleReplaceInTxtbox(/Conclusion(:?) ?/g, "Podsumowanie\1");
+        simpleReplaceInTxtbox(/Summary(:?) ?/g, "Podsumowanie\1");
+        simpleReplaceInTxtbox(/Express your answer in the form/g, "Wyraź swoją odpowiedź w postaci");
+        simpleReplaceInTxtbox(/ is real and complex/g, " jest rzeczywista i zespolona");
+        simpleReplaceInTxtbox(/ is pure imaginary and complex/g, " jest tylko urojona i zespolona");
+        simpleReplaceInTxtbox(/ is complex/g, " jest zespolone");
+        simpleReplaceInTxtbox(/ unit /g, " jednostka");
+        simpleReplaceInTxtbox(/ units /g, " jednostki(ek) ");
+        simpleReplaceInTxtbox(/ units$/g, " jednostki(ek)");
+        simpleReplaceInTxtbox(/ unit$/g, " jednostka");
+        simpleReplaceInTxtbox(/\bAnswer\b/g, "Odpowiedź");
+        simpleReplaceInTxtbox(/ and so /g, ", a więc: ");
+        simpleReplaceInTxtbox(/ and /g, " i ");
+        simpleReplaceInTxtbox(/ or /g, " lub ");
+        simpleReplaceInTxtbox(/\bThis is\b/g, "To jest");
+        simpleReplaceInTxtbox(/\bSolving for\b/g, "Rozwiązując dla");
+        simpleReplaceInTxtbox(/ for /g, " dla ");
+        simpleReplaceInTxtbox(/ converges/g, " zbiega");
+        simpleReplaceInTxtbox(/ diverges/g, " rozbieżny");
+        simpleReplaceInTxtbox(/Only/g, "Tylko");
+        simpleReplaceInTxtbox(/\bYes\b/g, "Tak");
+        simpleReplaceInTxtbox(/\byes\b/g, "tak");
+        simpleReplaceInTxtbox(/\bNo\b/g, "Nie");
+        simpleReplaceInTxtbox(/\bno\b/g, "nie");
+        simpleReplaceInTxtbox(/\bbut\b/g, "ale");
+        simpleReplaceInTxtbox(/\bBut\b/g, "Ale");
+        simpleReplaceInTxtbox(/\bpoints\b/g, "punktów");
+        simpleReplaceInTxtbox(/\bpoint\b/g, "punkt");
+        simpleReplaceInTxtbox(/\b[Ss]catterplot\b/g, "Wykres punktowy");
+        simpleReplaceInTxtbox(/\bFill in the blank\b/g, "Wypełnij puste pola");
    }
 
    /**
@@ -102,7 +98,7 @@
     */
    function altWAction() {    
         var expr = /\$([A-Z]?\{?)\(\s*(-?\d+(([\.,]|\{,\})\d+)?|-?[a-z]|-?\\\\[a-z]+[A-Z]?\{-?\d+[.,]?\d*\})\s*[,;|]\s*(-?\d+(([\.,]|\{,\})\d+)?|-?[a-z]|-?\\\\[a-z]+[A-Z]?\{-?\d+[.,]?\d*\})\s*\)(\}?)\$/g;
-        simpleReplaceInTxtbox(expr, "$$$1($2{\\,}|{\\,}$5)$8$$");
+        simpleReplaceInTxtbox(expr, "$$$1($2{\\,}{\\;}{\\,}$5)$8$$");
    }
 
    function altUAction() {
@@ -130,14 +126,14 @@
       txtBox.innerHTML = txtBox.innerHTML.replace(/numerische Eingabe/g,'numeric-input'); 
       txtBox.innerHTML = txtBox.innerHTML.replace(/numerische-Eingang/g,'numeric-input');    
       txtBox.innerHTML = txtBox.innerHTML.replace(/☃ Bild/g,'☃ image');
-      txtBox.innerHTML = txtBox.innerHTML.replace('**How','**Wie');
-      txtBox.innerHTML = txtBox.innerHTML.replace('**What','**Was');
-      txtBox.innerHTML = txtBox.textContent.replace(/\\text{ ones}}/g,'\\text{ Einer}}');
-      txtBox.innerHTML = txtBox.textContent.replace(/\\text{ one}}/g,'\\text{ Einer}}');
-      txtBox.innerHTML = txtBox.textContent.replace(/\\text{ tens}}/g,'\\text{ Zehner}}');
-      txtBox.innerHTML = txtBox.textContent.replace(/\\text{ ten}}/g,'\\text{ Zehner}}');
-      txtBox.innerHTML = txtBox.textContent.replace(/\\text{ hundred}}/g,'\\text{ Hunderter}}');
-      txtBox.innerHTML = txtBox.textContent.replace(/\\text{ hundreds}}/g,'\\text{ Hunderter}}');
+      txtBox.innerHTML = txtBox.innerHTML.replace('**How','**Jak');
+      txtBox.innerHTML = txtBox.innerHTML.replace('**What','**Co');
+      txtBox.innerHTML = txtBox.textContent.replace(/\\text{ ones}}/g,'\\text{ jedności}}');
+      txtBox.innerHTML = txtBox.textContent.replace(/\\text{ one}}/g,'\\text{ jedność}}');
+      txtBox.innerHTML = txtBox.textContent.replace(/\\text{ tens}}/g,'\\text{ dziesiątki(ek)}}');
+      txtBox.innerHTML = txtBox.textContent.replace(/\\text{ ten}}/g,'\\text{ dziesiątka}}');
+      txtBox.innerHTML = txtBox.textContent.replace(/\\text{ hundred}}/g,'\\text{ setka}}');
+      txtBox.innerHTML = txtBox.textContent.replace(/\\text{ hundreds}}/g,'\\text{ setek(ki)}}');
       
       //Not sure what alanis stuff above should do, but it doesn't work. This one does.
       simpleReplaceInTxtbox(/!\s+\[\]\s+\(/g, "![](");
